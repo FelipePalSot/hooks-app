60896504ffcd9c3827136ccaa5f20991a81c8df8
Felipe Jean Franco Palomino Sotelo <felipejeanfrancopalominosotelo@MacBook-Pro-de-Felipe.local>
2026-01-10 22:22:32 -0500

    feat(): new fucnatilites

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { HooksApp } from './HooksApp'
+import { HooksApp } from './HooksApp';
+import {TrafficLight} from '../src/01-useState/TrafficLight';
+import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect';
 
 import './index.css'
 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HooksApp></HooksApp>
+    {/* <HooksApp></HooksApp> */}
+      {/* <TrafficLight/> */}
+      <TrafficLightWithEffect/>
   </StrictMode>,
 )
