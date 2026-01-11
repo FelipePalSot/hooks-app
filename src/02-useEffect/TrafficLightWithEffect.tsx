3ff09a1b0a12746e19b7b57a18a68c6e8e390cc9
Felipe Jean Franco Palomino Sotelo <felipejeanfrancopalominosotelo@MacBook-Pro-de-Felipe.local>
2026-01-11 07:55:18 -0500

    add hooks for the traffic light

--- src/02-useEffect/TrafficLightWithEffect.tsx
+++ src/02-useEffect/TrafficLightWithEffect.tsx
@@ -17,21 +17,48 @@ export const TrafficLightWithEffect = () => {
   const [light, setLight] = useState<TrafficLightColor>('red'); // red seria el estado inicial
   const [countdown, setCountdown] = useState(5);
 
+  //useEffect para el countdown
   useEffect(()=>{
-    if( countdown === 0 ) return;
-    console.log({countdown});
+    if(countdown === 0) return;
+    
+    //console.log({countdown});
 
     const intervalId = setInterval(()=>{
-      console.log('setInterval llamado');
+      //console.log('setInterval llamado');
       setCountdown((prev)=>prev-1);
     }, 1000);
 
     return()=>{
-      console.log('Cleanup effect');
+      //console.log('Cleanup effect');
       clearInterval(intervalId);
     }
 
   }, [countdown]); //useEffect renderiza cada vez que cambia una dependecia
+
+  // Change light color effect
+  useEffect(() =>{
+    if( countdown > 0 ) return;
+      console.log("iniciar")
+      setCountdown(5);
+      if(light === 'red'){
+         console.log("voy a green")
+        setLight('green');
+        return;
+      }
+      if(light === 'yellow'){
+         console.log("voy a red")
+        setLight('red');
+        return;
+      }
+      if(light === 'green'){
+        console.log("voy a yellow")
+        setLight('yellow');
+        return;
+      }
+      return;
+    
+
+  },[countdown]);
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
@@ -40,6 +67,13 @@ export const TrafficLightWithEffect = () => {
         <h1 className="text-white text-3xl font-thin">Semáforo con useEffect</h1>
 
         <h2 className="text-white text-xl"> Countdown {countdown} </h2>
+
+        <div className="w-64 bg-gray-700 rounded-full h-2">
+          <div
+            className="bg-blue-500 h-2 rounded-full transition-all duration-100 ease-linear"
+            style={{width: `${(countdown / 5)*100}%`}}
+          ></div>
+        </div>
 
         <div
           className={`w-32 h-32 ${
--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,16 @@ import {TrafficLight} from '../src/01-useState/TrafficLight';
 import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect';
 
 import './index.css'
+import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook';
+import { PokemonPage } from './03-examples/PokemonPage';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <HooksApp></HooksApp> */}
       {/* <TrafficLight/> */}
-      <TrafficLightWithEffect/>
+      {/* <TrafficLightWithEffect/> */}
+      {/* <TrafficLightWithHook/> */}
+      <PokemonPage/>
   </StrictMode>,
 )
